6281f582f213b9150240da235fa8bf4089b78615
Jason Chen <jche774@aucklanduni.ac.nz>
2023-10-30 19:53:34 +1300

    feat: Register models to admin

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Ingredient, IngredientRequirement, MenuItem, Purchase
+
 
 # Register your models here.
+admin.site.register(Ingredient)
+admin.site.register(IngredientRequirement)
+admin.site.register(MenuItem)
+admin.site.register(Purchase)
