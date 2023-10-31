43eb54e3bdf249ae171a1b829468dc7f6e480aa7
Jason Chen <jche774@aucklanduni.ac.nz>
2023-10-31 13:31:20 +1300

    feat: Implement logout functionality

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='index'),
+    path('logout/', views.logout_view, name='logout'),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
+from django.contrib.auth import logout
 
 # Create your views here.
 class HomeView(TemplateView):
   template_name = "restaurant/home.html"
+
+def logout_view(request):
+   logout(request)
+   return redirect("index")
