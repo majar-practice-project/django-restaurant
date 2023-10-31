245a04fb8cea5b4a068e7e4dd84f2598bd57c1a9
Jason Chen <jche774@aucklanduni.ac.nz>
2023-11-01 02:14:46 +1300

    feat: Add navigation and authorization to adding ingredient

--- restaurant/views.py
+++ restaurant/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.views.generic import TemplateView, ListView, DetailView, CreateView
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView
 
 from .models import Ingredient, IngredientRequirement, MenuItem, Purchase
@@ -16,11 +17,11 @@ class HomeView(TemplateView):
 class Login(LoginView):
    redirect_authenticated_user = True
 
-   def get_success_url(self):
-    if self.request.user.is_authenticated:
-        return reverse('index')
-    else:
-        return reverse('login')
+#    def get_success_url(self):
+#     if self.request.user.is_authenticated:
+#         return reverse('index')
+#     else:
+#         return reverse('login')
 
 def logout_view(request):
    logout(request)
@@ -30,7 +31,7 @@ class Ingredients(ListView):
    model = Ingredient
    template_name = 'restaurant/inventory.html'
 
-class CreateIngredient(CreateView):
+class CreateIngredient(LoginRequiredMixin, CreateView):
    model = Ingredient
    template_name = 'restaurant/form.html'
    form_class = forms.IngredientForm
