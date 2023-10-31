5e6f4f50286f83f656fcffa2eda7a212957eff3a
Jason Chen <jche774@aucklanduni.ac.nz>
2023-10-31 13:19:20 +1300

    feat: Add template to home view

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
-def home(request):
-    return HttpResponse('Home')
+class HomeView(TemplateView):
+  template_name = "restaurant/home.html"
