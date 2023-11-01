541f88b2317a9aa3e6c6602ae3c116481fd675a0
Jason Chen <jche774@aucklanduni.ac.nz>
2023-11-01 19:01:48 +1300

    feat: Implement delete view for menu item

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('menu/<pk>/ingredients/<fk>/delete', views.DeleteMenuIngredient.as_view(), name='delete_menu_item_ingredient'),
 
     path('menu/<pk>/update', views.UpdateMenuItem.as_view(), name='update_menu_item'),
+    path('menu/<pk>/delete', views.DeleteMenuItem.as_view(), name='delete_menu_item'),
 ]
